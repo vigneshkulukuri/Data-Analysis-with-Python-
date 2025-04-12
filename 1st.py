271b6a019612796c216658c79a514bc120e0196b
VIGNESH KULUKURI <vigneshvicky8359@gmail.com>
2025-04-12 12:21:30 +0530

    Update 1st.py

--- 1st.py
+++ 1st.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os
 
 df = pd.read_csv(r"V:\ecommerce_customer_data_custom_ratios.csv")
 # Convert 'Purchase Date' to datetime format
